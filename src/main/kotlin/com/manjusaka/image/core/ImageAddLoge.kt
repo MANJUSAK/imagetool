be481ff9328051a87594f8c331230734967317cb
Lenovo <manjusakachn@yeah.net>
2019-01-12 14:19:15 +0800

    Initial commit

---
+++ src/main/kotlin/com/manjusaka/image/core/ImageAddLoge.kt
@@ -0,0 +1,77 @@
+package com.manjusaka.image.core
+
+import com.sun.image.codec.jpeg.JPEGCodec
+import java.awt.image.BufferedImage
+import java.io.File
+import java.io.FileNotFoundException
+import java.io.FileOutputStream
+import java.io.OutputStream
+import javax.imageio.ImageIO
+
+/**
+ * description:
+ * ===>图片批量添加logo工具程序
+ *
+ * @author manjusaka[manjusakachn@gmail.com] Created on 2019-01-12 12:12
+ * @version V1.0.0
+ */
+object ImageAddLoge {
+    /**
+     * @param photopath : 原图存放的文件夹路径
+     * @param logopath : logo图像存放的路径
+     * @param savepath : 目标输出保存的文件夹路径
+     * @param x : logo图像在合并到原图中的显示位置x座标
+     * @param y : logo图像在合并到原图中的显示位置y座标
+     */
+    fun findFileList(photoFolderPath: String, logoPath: String, saveFolderPath: String, x: Int, y: Int) {
+        if (!File(logoPath).exists()) throw FileNotFoundException("$logoPath notFind file")
+        val file = File(photoFolderPath)
+        if (!file.exists()) throw FileNotFoundException("$photoFolderPath notFind folder or not is folder")
+        val saveFile = File(saveFolderPath)
+        if (!saveFile.exists()) {
+            saveFile.mkdirs()
+        }
+        val fileList = file.listFiles()
+        val pfpath = photoFolderPath + "\\"
+        val sfpath = saveFolderPath + "\\"
+        fileList.forEach {
+            //添加logo  原图片路径  logo路径 生成合成图片路径 x,y
+            addImageLogo(pfpath + it.name, logoPath, sfpath + it.name, x, y)
+        }
+        println("成功添加" + fileList.size + "张图片水印logo")
+        Runtime.getRuntime().exec("cmd /c start explorer $saveFolderPath")
+    }
+
+    /**
+     * @param photopath : 原图存放的路径
+     * @param logopath : logo图像存放的路径
+     * @param savepath : 目标输出保存的路径
+     * @param x : logo图像在合并到原图中的显示位置x座标
+     * @param y : logo图像在合并到原图中的显示位置y座标
+     */
+    fun addImageLogo(photopath: String, logopath: String, savepath: String, x: Int, y: Int) {
+        var os: OutputStream? = null
+        try {
+            val image = ImageIO.read(File(photopath))
+            val pwidth = image.width
+            val pheight = image.height
+            val buffimage = BufferedImage(pwidth, pheight, BufferedImage.TYPE_INT_BGR)
+            val g = buffimage.createGraphics()
+            g.drawImage(image, 0, 0, pwidth, pheight, null)
+
+            val logo = ImageIO.read(File(logopath))
+            val lwidth = logo.width
+            val lheight = logo.height
+            g.drawImage(logo, pwidth - x, pheight - y, lwidth, lheight, null)
+
+            g.dispose()
+            os = FileOutputStream(savepath)
+            val encoder = JPEGCodec.createJPEGEncoder(os)
+            encoder.encode(buffimage)
+        } catch (e: Exception) {
+            throw Exception("Add image watermark exception:" + e.message)
+        } finally {
+            os?.close()
+        }
+    }
+}
---
+++ src/test/kotlin/ImageAddLogeTest.kt
@@ -0,0 +1,15 @@
+import com.manjusaka.image.core.ImageAddLoge
+
+/**
+ * description:
+ * ===>
+ * company
+ * @author manjusaka[manjusakachn@gmail.com] Created on 2019-01-12 14:04
+ * @version V1.0.0
+ */
+fun main(args: Array<String>) {
+    val yLj = "C:\\Users\\Lenovo\\Desktop\\temp\\snap\\"
+    val logoPath = "C:\\Users\\Lenovo\\Desktop\\logo.jpg"
+    val savePath = "C:\\Users\\Lenovo\\Desktop\\temp\\test\\"
+    ImageAddLoge.findFileList(yLj, logoPath, savePath, 550, 550)
+}
